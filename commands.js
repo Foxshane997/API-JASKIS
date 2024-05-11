e283230d22cef6dcd8353e0eefc29e2f48304333
“Foxshane997” <Foxshane997@gmail.com>
2024-05-11 13:55:07 -0700

    updating

--- commands.js
+++ commands.js
@@ -47,7 +47,7 @@
 
 // 4. Query for a Groundhog in the Woodlands
 
-// db.bounties.find({ species: "Groundhog", location: "Woodlands" })
+// db.bounties.find({ species: Groundhog, location: Woodlands })
 
 // Update and Delete
 // 1. Update the reward for Polarwind to 10000
